3a67c06dcace02d8ae20abbd53df15943cd15ef2
chansuke <moonset20@gmail.com>
2023-10-09 16:05:04 +0900

    feature: add `printf` function

--- kernel.c
+++ kernel.c
@@ -1,4 +1,5 @@
 #include "kernel.h"
+#include "common.h"
 
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
@@ -41,11 +42,10 @@ void *memset(void *buf, char c, size_t n) {
 
 /* memset関数を使って.bss領域をゼロで初期化 */
 void kernel_main(void) {
-    const char *s = "\n\nHello World!\n";
-    for (int i = 0; s[i] != '\0'; i++) {
-        putchar(s[i]);
-    }
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+
+    printf("\n\nHello %s\n", "World!");
+    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
     for (;;) {
         __asm__ __volatile__("wfi");
